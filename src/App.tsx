e51004ec6e5085f4a16fd42b79a6773f4204c2f3
Henri Evjen <henrievjen@gmail.com>
2019-09-22 23:57:37 -0500

    added resume document

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,12 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="head">
-        <h1 className="header-name mt-1 mb-5">Henri Evjen</h1>
+        <h1 className="header-name mt-2 mb-5">Henri Evjen</h1>
       </header>
       <div className="row main">
         <div className="resume col-xl-4 text-center">
           <h2 className="section-header">Resume</h2>
+          <iframe className="resume-document" src="https://docs.google.com/document/d/1_xlyQjHw4ylexbQIBCXaS4U4tRdroPg76H4ah6nfROc/edit?usp=sharing"></iframe>
         </div>
         <div className="profile col-xl-4 text-center border-left border-right border-light">
           <h2 className="section-header">Profile</h2>
