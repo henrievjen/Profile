8d9fd41b1e453abbf4dce449189b40a2ca88c24a
Henri Evjen <henrievjen@gmail.com>
2019-09-24 23:21:43 -0500

    resume image working and no more scroll-bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import resume_image from './images/resume.png';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="head">
-        <h1 className="header-name mt-2 mb-5">Henri Evjen</h1>
-      </header>
-      <div className="row main">
-        <div className="resume col-xl-4 text-center">
-          <h2 className="section-header">Resume</h2>
-          <iframe className="resume-document" src="https://docs.google.com/document/d/1_xlyQjHw4ylexbQIBCXaS4U4tRdroPg76H4ah6nfROc/edit?usp=sharing"></iframe>
-        </div>
-        <div className="profile col-xl-4 text-center border-left border-right border-light">
-          <h2 className="section-header">Profile</h2>
-        </div>
-        <div className="projects col-xl-4 text-center">
-          <h2 className="section-header">Projects</h2>
+    <div className="App text-center">
+      <div className="centered-main">
+        <header className="head">
+          <h1 className="header-name mt-2 mb-5">Henri Evjen</h1>
+        </header>
+        <div className="row main">
+          <div className="col-xl-4 text-center">
+            <h2 className="section-header">Resume</h2>
+            <img src={resume_image} className="resume rounded mt-xl-5" />
+          </div>
+          <div className="profile col-xl-4 text-center border-left border-right border-light">
+            <h2 className="section-header">Profile</h2>
+          </div>
+          <div className="projects col-xl-4 text-center">
+            <h2 className="section-header">Projects</h2>
+          </div>
         </div>
       </div>
     </div>
