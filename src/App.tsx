1a0afd410c29bd8c54051e6e24a35e8211403d6f
henrievjen <henrievjen@gmail.com>
2019-10-03 18:05:28 -0500

    added react routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Resume from './components/resume/Resume';
@@ -7,18 +8,25 @@ import Projects from './components/projects/Projects';
 
 const App: React.FC = () => {
   return (
-    <div className="App text-center">
-      <div className="centered-main">
-        <header className="head">
-          <h1 className="header-name mt-2 mb-5">Henri Evjen</h1>
-        </header>
-        <div className="row main">
-          <Resume></Resume>
-          <Profile></Profile>
-          <Projects></Projects>
+    <Router>
+      <div className="App text-center">
+        <div className="centered-main">
+          <header className="head">
+            <h1 className="header-name mt-2 mb-5">Henri Evjen</h1>
+          </header>
+          <div className="row main">
+            <Resume></Resume>
+            <Profile></Profile>
+            <Projects></Projects>
+          </div>
         </div>
       </div>
-    </div>
+
+      <Switch>
+        <Redirect from="/" to="/profile" />
+        <Route path="/" component={App}></Route>
+      </Switch>
+    </Router>
   );
 }
 
